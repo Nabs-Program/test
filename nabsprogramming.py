c597e6b28c37f56739a2c1ec2216de8088fbf217
Nabs-Program <nabilashash@gmail.com>
2025-05-08 09:32:03 +0700

    Add files via upload

---
+++ nabsprogramming.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+#memuat model regresi linear yang sudah disimpan
+lin_reg_loaded = joblib. load('lin_reg model joblib')
+
+# Judul aplikasi
+st.title("Prediksi Gaji Berdasarkan Lama Bekerja")
+
+# Input tahun pengalaman kerja
+years_experience = st.number_input("Masukkan jumlah tahun bekerja:", min_value=0.0, step=0.1)
+
+# Prediksi gaji
+if st. button ("Prediksi Gaji"):
+gaji = lin_ reg loaded. predict ([[years_experience]])
+st.write(f"Gaji seseorang setelah bekerja selama {years _experience} tahun adalah ${gaji [0]:,.2f}")
